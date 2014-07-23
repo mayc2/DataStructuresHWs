40a166d583709c3a9240d7fdf0a4a96822b3f1f1
mayc2 <mayc2@rpi.edu>
2014-07-23 15:48:36 -0400

    First Commit

---
+++ scripts/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cmath>
+#include <fstream>
+#include <iomanip>
+
+int main(){
+
+std::cout<<"test"<<std::endl;
+
+return 1;
+
+}
