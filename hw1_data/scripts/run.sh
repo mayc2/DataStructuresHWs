9137474a89ebda845c68ce4aad326800cc3c7536
mayc2 <mayc2@rpi.edu>
2014-07-29 16:43:34 -0400

    Created script to compile and run main.cpp

--- hw1_data/scripts/run.sh
+++ hw1_data/scripts/run.sh
@@ -1,4 +1,24 @@
-#include <iostream>
-using namespace std;
+ROOTDIR="/DataStructuresHWs/hw1_data"
+SCRIPTS_DIR="${ROOTDIR}/scripts"
+IO_DIR="${ROOTDIR}/input_output"
 
-cout << "hi";
+echo 'compiling'
+g++ main.cpp -o justify.exe
+
+echo 'executing flush left'
+${SCRIPTS_DIR}/justify.exe ${IO_DIR}/example.txt fl_out.txt 16 flush_left
+
+echo 'executing flush right'
+${SCRIPTS_DIR}/justify.exe ${IO_DIR}/example.txt fr_out.txt 16 flush_right
+
+echo 'executing full justify'
+${SCRIPTS_DIR}/justify.exe ${IO_DIR}/example.txt full_out.txt 16 full_justify
+
+RETURN_STATUS=$?
+if [ RETURN_STATUS -ne 0 ]; do
+	echo 'Failed... exiting...'
+	exit 1
+else
+	echo 'Success! exiting ...'
+	exit 0
+fi
