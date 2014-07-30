26e1b72ed1cd7ce1ba762cc056db9bc920471027
jko0531 <jko0531@gmail.com>
2014-07-30 16:58:22 -0400

    Update main.cpp

--- hw1_data/scripts/main.cpp
+++ hw1_data/scripts/main.cpp
@@ -36,6 +36,41 @@ int main(int argc, char const *argv[])
  	return 0;
 }
 
+void getLines_Needed(ifstream &in, const int &width){
+	
+	vector<string> words;
+	string word;
+	int lines_needed=1;
+	while (in>>word){
+		words.push_back(word);
+	}
+	
+	vector<string> formatted_words;
+	string formatted_word;
+	int line_count = 0;
+	for (int i = 0; i < words.size(); i++){
+		if (line_count + words[i].size() < width){
+			line_count += words[i].size();
+			formatted_word.append(words[i]);
+			formatted_word.append(" ");
+		}
+		else{
+			
+			formatted_words.push_back(formatted_word);
+			formatted_word = words[i] + ' ';
+			lines_needed++;
+			line_count = words[i].size();
+			if (i == words.size()-1) formatted_words.push_back(words[i]);
+		}
+		line_count++;
+		
+	}
+	
+	for (int i = 0; i < formatted_words.size(); i++){
+		cout << formatted_words[i] << "\n";
+	}
+}
+
 //reads input file into a vector using fstream
 std::vector<std::string> read_input_file(std::ifstream istr, const char * input_file){
 	if (!istr){
