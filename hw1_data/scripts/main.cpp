6e66784c71f05b8c6195e696ac01f3613e846bf8
mayc2 <mayc2@rpi.edu>
2014-07-30 16:49:58 -0400

    working on implementation of fstream

--- hw1_data/scripts/main.cpp
+++ hw1_data/scripts/main.cpp
@@ -4,29 +4,51 @@
 #include <cmath>
 #include <fstream>
 #include <iomanip>
+#include <cstdlib>
+//Note: exit codes
+//       Success = 0
+//       Failure = 1
 
 //Function Declarations
 void print_line(const int &width);
 void print(const std::vector<std::string> &lines,const int &width);
+std::vector<std::string> read_input_file(std::ifstream istr, const char * input_file);
 
 int main(int argc, char const *argv[])
 {
 	if (argc != 5){
 		std::cerr<<"Usage: file_name input_file output_file width style"<<std::endl;
-		return -1;
+		return 1;
+	}
+	
+	//CLI arguments implementation
+	std::ifstream istr(argv[1]);
+	std::ofstream ostr(argv[2]);
+	std::string style=argv[4];
+	const char * tmp=argv[3];
+	float width=atof(tmp);
+
+	std::vector<std::string> words = read_input_file(istr,argv[1]);
+	for(int i=0; i<words.size(); ++i){
+		std::cout<<words[i]<<std::endl;
 	}
 
-	//CLI variable value initialization
-	std::string file_name=argv[0];
-	std::string input_file=argv[1];
-	std::string output_file=argv[2];
-	std::string width=argv[3];
-	std::string style=argv[4];
-	
-	//check variables were initialized properly
-	std::cout<<"file_name="<<file_name<<"; input_file="<<input_file<<"; output_file="<<output_file<<"; width="<<width<<"; style="<<style<<std::endl;
-
  	return 0;
+}
+
+//reads input file into a vector using fstream
+std::vector<std::string> read_input_file(std::ifstream istr, const char * input_file){
+	if (!istr){
+		std::cerr<<"Could not open input_file, "<<input_file<<std::endl;
+		return;
+	}
+
+	std::string temp;
+	std::vector<std::string> words;
+	while(istr>>temp){
+		words.push_back(temp);
+	}
+
 }
 
 void print_line(const int &width){
@@ -44,3 +66,4 @@ void print(const std::vector<std::string> &lines,const int &width){
 	//print the bottom line
 	print_line(width);
 }
+
