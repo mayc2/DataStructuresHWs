26d1e53352e273922665bb832cbcc66f923b8a76
mayc2 <mayc2@rpi.edu>
2014-08-01 13:30:48 -0400

    addded in Player and Team classes (Note: Need to implement)

--- hw2_data/scripts/main.cpp
+++ hw2_data/scripts/main.cpp
@@ -5,10 +5,18 @@
 #include <cmath>
 #include <iomanip>
 #include <fstream>
+#include "Player.h"
+#include "Team.h"
 
 int main(int argc, char const *argv[])
 {
-	
+	//check to ensure correct number of arguments
+	if (argc != 3){
+		std::cerr<<"Usage: ./file_name.exe input_file output_file"<<std::endl;
+		return 1;
+	}	
+
+	parse_file();
 
 	return 0;
 }
