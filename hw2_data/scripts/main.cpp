3ae35fc4c10aefee96734ac4d74655ab495842f3
mayc2 <mayc2@rpi.edu>
2014-08-01 12:57:56 -0400

    added i/o files, directory structure, and main.cpp

---
+++ hw2_data/scripts/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <iomanip>
+#include <fstream>
+
+int main(int argc, char const *argv[])
+{
+	
+
+	return 0;
+}
